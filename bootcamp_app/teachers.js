7719e1dbb87b2fd4502cbb1f19df6da8e04fb292
Adam <adamgrharvey@gmail.com>
2022-07-28 00:37:28 -0700

    replaces students.js and teachers.js files with parameterized queries versions

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,21 +7,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
-GROUP BY teachers.name, cohorts.name
-ORDER BY teacher;
-`)
-.then(res => {
+const queryString = `
+  SELECT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  GROUP BY teachers.name, cohorts.name
+  ORDER BY teacher;
+  `;
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
+  .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`); 
   })
 }).catch(err => {
   console.log(err);
 });
-
